c54071c119fe8a1e8c24ba758e1b8e47f97dc8b6
ChristianVarela <christian_varela@outlook.com.br>
2023-03-07 00:37:52 -0300

    build: build do router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
