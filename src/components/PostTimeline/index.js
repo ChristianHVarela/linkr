015a873cce01768217caa5058078d9ca1b2a30e1
encarnacao <cencarnacaoq@gmail.com>
2023-03-09 19:07:31 -0300

    feat: add modal and styling

--- src/components/PostTimeline/index.js
+++ src/components/PostTimeline/index.js
@@ -1,9 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { ReactTagify } from "react-tagify";
-import {FaTrashAlt, FaPencilAlt} from "react-icons/fa";
+import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
 import * as S from "./styles";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement("#root")
 
 const PostTimeline = (props) => {
+	const [modalIsOpen, setModalIsOpen] = useState(false);
 	const { post } = props;
 	const navigate = useNavigate();
 	const handleClick = () => {
@@ -11,34 +16,79 @@ const PostTimeline = (props) => {
 	};
 
 	return (
-		<S.ContainerPost>
-			<S.ContainerImageProfile>
-				<S.ImageProfile src={post.image_profile} alt="" />
-			</S.ContainerImageProfile>
-			<S.ContainerContent>
-				<S.PostTop>
-					<S.UserName>{post.user_name}</S.UserName>
+		<>
+			<S.ContainerPost>
+				<S.ContainerImageProfile>
+					<S.ImageProfile src={post.image_profile} alt="" />
+				</S.ContainerImageProfile>
+				<S.ContainerContent>
+					<S.PostTop>
+						<S.UserName>{post.user_name}</S.UserName>
+						<div>
+							<FaPencilAlt />
+							<FaTrashAlt onClick={() => setModalIsOpen(true)} />
+						</div>
+					</S.PostTop>
 					<div>
-                        <FaPencilAlt />
-                        <FaTrashAlt />
-                    </div>
-				</S.PostTop>
-				<div>
-					<ReactTagify
-						tagStyle={{ fontWeight: "bold", cursor: "pointer" }}
-						tagClicked={(tag) =>
-							navigate("/hashtag/" + tag.slice(1))
-						}
-					>
-						<S.Description>{post.description}</S.Description>
-					</ReactTagify>
-				</div>
-				<S.ContainerMetadata
-					onClick={handleClick}
-				></S.ContainerMetadata>
-			</S.ContainerContent>
-		</S.ContainerPost>
+						<ReactTagify
+							tagStyle={{ fontWeight: "bold", cursor: "pointer" }}
+							tagClicked={(tag) =>
+								navigate("/hashtag/" + tag.slice(1))
+							}
+						>
+							<S.Description>{post.description}</S.Description>
+						</ReactTagify>
+					</div>
+					<S.ContainerMetadata
+						onClick={handleClick}
+					></S.ContainerMetadata>
+				</S.ContainerContent>
+			</S.ContainerPost>
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={() => setModalIsOpen(false)}
+				style={modalStyle}
+				contentLabel="Example Modal"
+			>
+				<S.ModalDiv>
+					<S.ModalText>
+						Are you sure you want to delete this post?
+					</S.ModalText>
+					<div>
+						<S.ModalButton
+							className="cancel"
+							onClick={() => setModalIsOpen(false)}
+						>
+							No, go back
+						</S.ModalButton>
+						<S.ModalButton
+							className="confirm"
+							onClick={() => setModalIsOpen(false)}
+						>
+							Yes, delete it
+						</S.ModalButton>
+					</div>
+				</S.ModalDiv>
+			</Modal>
+		</>
 	);
+};
+
+const modalStyle = {
+	content: {
+		top: "50%",
+		left: "50%",
+		right: "auto",
+		bottom: "auto",
+		marginRight: "-50%",
+		transform: "translate(-50%, -50%)",
+		backgroundColor: "#333333",
+		borderRadius: "50px 130px",
+		padding: "40px",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	},
 };
 
 export default PostTimeline;
--- src/components/PostTimeline/styles.js
+++ src/components/PostTimeline/styles.js
@@ -1,80 +1,133 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ContainerPost = styled.div`
-    width: 100%;
-    min-height: 275px;
-    border-radius: 16px;
-    background: #171717;
-    margin-bottom: 15px;
-    display: flex;
-    justify-content: space-between;
-`
+	width: 100%;
+	min-height: 275px;
+	border-radius: 16px;
+	background: #171717;
+	margin-bottom: 15px;
+	display: flex;
+	justify-content: space-between;
+`;
 export const ContainerImageProfile = styled.div`
-    @media (max-width: 710px){
-        display: none;
-    }
-`
+	@media (max-width: 710px) {
+		display: none;
+	}
+`;
 export const ImageProfile = styled.img`
-    width: 50px;
-    height: 50px;
-    border-radius: 26.5px;
-    margin-top: 17px;
-    margin-left: 17px;
-    object-fit: cover;
-`
+	width: 50px;
+	height: 50px;
+	border-radius: 26.5px;
+	margin-top: 17px;
+	margin-left: 17px;
+	object-fit: cover;
+`;
 export const ContainerContent = styled.div`
-    width: 500px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-right: 1rem;
-`
+	width: 500px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	margin-right: 1rem;
+`;
 export const UserName = styled.p`
-    width: 100%;
-    height: 23px;
-    font-family: "Lato";
-    font-weight: 400;
-    font-size: 19px;
-    line-height: 22px;
-    color: #FFFFFF;
-    margin-top: 20px;
-`
+	width: 100%;
+	height: 23px;
+	font-family: "Lato";
+	font-weight: 400;
+	font-size: 19px;
+	line-height: 22px;
+	color: #ffffff;
+	margin-top: 20px;
+`;
 export const Description = styled.p`
-    width: 100%;
-    min-height: 45px;
-    font-family: "Lato";
-    font-weight: 400;
-    font-size: 17px;
-    line-height: 20px;
-    color: #B7B7B7;
-    margin-top: 10px;
-`
+	width: 100%;
+	min-height: 45px;
+	font-family: "Lato";
+	font-weight: 400;
+	font-size: 17px;
+	line-height: 20px;
+	color: #b7b7b7;
+	margin-top: 10px;
+`;
 export const ContainerMetadata = styled.div`
-    width: 100%;
-    height: 155px;
-    background: #171717;
-    border-radius: 11px;
-    border: 1px solid #C4C4C4;
-    margin-bottom: 20px;
-    margin-top: 1rem;
-    cursor: pointer;
-`
+	width: 100%;
+	height: 155px;
+	background: #171717;
+	border-radius: 11px;
+	border: 1px solid #c4c4c4;
+	margin-bottom: 20px;
+	margin-top: 1rem;
+	cursor: pointer;
+`;
 
 export const PostTop = styled.div`
-    width: 100%;
-    height: 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 5px;
-    > div {
-        display: flex;
-        align-items: center;
+	width: 100%;
+	height: 50px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 5px;
+	> div {
+		display: flex;
+		align-items: center;
+	}
+	> div > svg {
+		margin-left: 10px;
+		color: white;
+		font-size: 15px;
+		cursor: pointer;
+	}
+`;
+
+export const ModalDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+    max-width: 400px;
+    margin: 0 80px;
+    @media (max-width: 768px) {
+        margin: 0 20px;
     }
-    > div > svg {
-        margin-left: 10px;
-        color: white;
-        font-size: 15px;
-        cursor: pointer;
+`;
+
+export const ModalText = styled.p`
+	font-family: "Lato", sans-serif;
+	font-weight: 700;
+	font-size: 34px;
+	line-height: 41px;
+	color: #ffffff;
+	text-align: center;
+	margin-bottom: 40px;
+    @media (max-width: 768px) {
+       font-size: 24px;
+    }
+`;
+
+export const ModalButton = styled.button`
+	width: 150px;
+	height: 40px;
+	border-radius: 5px;
+	border: none;
+	margin-right: 20px;
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 700;
+	font-size: 18px;
+	line-height: 22px;
+    cursor: pointer;
+	&.confirm {
+		background: #1877f2;
+		color: white;
+	}
+	&.cancel {
+		color: #1877f2;
+		background: white;
+	}
+    @media (max-width: 768px) {
+        height: 40px;
+        width: 100px;
+        margin-right: 14px;
+        font-size: 16px;
     }
 `;
