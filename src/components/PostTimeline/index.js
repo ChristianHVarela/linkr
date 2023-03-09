599c994f0b2ed40a7f9d183f5a6910960900679c
encarnacao <cencarnacaoq@gmail.com>
2023-03-09 17:41:01 -0300

    feat: use react-tagify to style tags

--- src/components/PostTimeline/index.js
+++ src/components/PostTimeline/index.js
@@ -1,28 +1,35 @@
-import * as S from "./styles"
+import { useNavigate } from "react-router-dom";
+import { ReactTagify } from "react-tagify";
+import * as S from "./styles";
 
 const PostTimeline = (props) => {
-    const { post } = props
+	const { post } = props;
+    const navigate = useNavigate();
+	const handleClick = () => {
+		window.open(post.link, "_blank");
+	};
 
-    const handleClick = () => {
-        window.open(post.link, "_blank");
-      }
+	return (
+		<S.ContainerPost onClick={handleClick}>
+			<S.ContainerImageProfile>
+				<S.ImageProfile src={post.image_profile} alt="" />
+			</S.ContainerImageProfile>
+			<S.ContainerContent>
+				<div>
+					<S.UserName>{post.user_name}</S.UserName>
+					<ReactTagify
+						tagStyle={{ fontWeight: "bold", cursor: "pointer" }}
+						tagClicked={(tag) =>
+							(navigate("/hashtag/" + tag.slice(1)))
+						}
+					>
+						<S.Description>{post.description}</S.Description>
+					</ReactTagify>
+				</div>
+				<S.ContainerMetadata></S.ContainerMetadata>
+			</S.ContainerContent>
+		</S.ContainerPost>
+	);
+};
 
-    return (
-        <S.ContainerPost onClick={handleClick}>
-            <S.ContainerImageProfile>
-                <S.ImageProfile src={post.image_profile} alt="" />
-            </S.ContainerImageProfile>
-            <S.ContainerContent>
-                <div>
-                    <S.UserName>{post.user_name}</S.UserName>
-                    <S.Description>{post.description}</S.Description>
-                </div>
-                <S.ContainerMetadata>
-
-                </S.ContainerMetadata>
-            </S.ContainerContent>
-        </S.ContainerPost>
-    )
-}
-
-export default PostTimeline
+export default PostTimeline;
