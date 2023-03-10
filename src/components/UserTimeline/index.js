0e1c00e39a1614897a608ec00e1ef6a55fb48281
expeditus303 <ricardotonietto@gmail.com>
2023-03-10 15:06:29 -0300

    fix:problem prior merge

--- src/components/UserTimeline/index.js
+++ src/components/UserTimeline/index.js
@@ -1,4 +1,4 @@
-import { ThreeDots } from "react-loader-spinner"
+import { ThreeDots } from "react-loader-spinner" 
 import UserPostTimeline from "../UserPostTimeline"
 import * as S from "./styles"
 
