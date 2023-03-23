828bfa0a22043f74c1aab602855b3b624bcd820e
encarnacao <cencarnacaoq@gmail.com>
2023-03-23 17:32:01 -0300

    feat: checks for new posts

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,4 +1,6 @@
 import { useContext, useEffect, useState } from "react"
+import useInterval from "use-interval"
+import ReloadPosts from "../../components/NewPosts"
 import PostCreation from "../../components/PostCreation"
 import Timeline from "../../components/Timeline"
 import Trending from "../../components/Trending"
@@ -9,21 +11,30 @@ import * as S from "./styles"
 const Posts = () => {
     const { image, config, update } = useContext(AuthContext)
     const [posts, setPosts] = useState(null)
+    const [newPosts, setNewPosts] = useState(null);
+    const newCount = posts ? newPosts.map(e=>e.id).indexOf(posts[0].id): null;
+    console.log(newCount);
 
     useEffect(() => {
         getPosts();
-        const interval = setInterval(() => {
-            getPosts();
-        }, 5000);
-        return () => clearInterval(interval);
     }, [update]);
+
+    useInterval(async () =>{
+        try{
+            const { data } = await api.get('/timeline', config);
+            setNewPosts(data);
+        } catch (e){
+            console.log(`Couldn't fetch new posts: ${e}`);
+        }
+    }, 15000);
 
     const getPosts = async () => {
         try {
-            const { data } = await api.get('/timeline', config)
-            setPosts(data)
+            const { data } = await api.get('/timeline', config);
+            setPosts(data);
+            setNewPosts(data);
         } catch (error) {
-            alert('An error occured while trying to fetch the posts, please refresh the page')
+            alert('An error occured while trying to fetch the posts, please refresh the page');
         }
     }
 
@@ -32,6 +43,7 @@ const Posts = () => {
             <S.ContainerCenter>
                 <S.Title>timeline</S.Title>
                 <PostCreation imageUrl={image} headers={config} />
+                {newCount > 0 && <ReloadPosts postsCount={newCount} />}
                 <Timeline posts={posts} />
             </S.ContainerCenter>
             <S.ContainerTrending>
