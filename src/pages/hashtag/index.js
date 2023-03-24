cf9f3b2b92ffe561c9a8d1a73b3926a3651ae79f
encarnacao <cencarnacaoq@gmail.com>
2023-03-24 18:01:21 -0300

    feat: add infinite scroller for hashtag timeline

--- src/pages/hashtag/index.js
+++ src/pages/hashtag/index.js
@@ -1,5 +1,7 @@
 import { useContext, useEffect, useState } from "react"
+import InfiniteScroll from "react-infinite-scroller"
 import { useParams } from "react-router-dom"
+import LoadingScroller from "../../components/LoadingScroller"
 import Timeline from "../../components/Timeline"
 import Trending from "../../components/Trending"
 import api from "../../config/api"
@@ -9,12 +11,15 @@ import * as S from "./styles"
 const HashtagPosts = () => {
     const { config, update } = useContext(AuthContext)
     const [posts, setPosts] = useState(null)
+    const [page, setPage] = useState(2);
+	const [hasMoreItems, setHasMoreItems] = useState(false);
     const { hashtag } = useParams();
 
     const getPosts = async () => {
         try {
             const { data } = await api.get(`/hashtag/${hashtag}`, config)
             setPosts(data)
+            setHasMoreItems(data.length === 10);
         } catch (error) {
             alert('An error occured while trying to fetch the posts, please refresh the page')
         }
@@ -22,13 +27,36 @@ const HashtagPosts = () => {
 
     useEffect(() => {
         getPosts();
+        setPage(2);
     }, [update,hashtag]);
+
+    const loadMore = async () => {
+		try {
+			const { data } = await api.get(`/hashtag/${hashtag}?page=${page}`, config);
+			setPosts([...posts, ...data]);
+			setPage(page + 1);
+			if(data.length < 10) {
+				setHasMoreItems(false);
+			}
+		} catch (e) {
+			console.log(`Couldn't fetch more posts: ${e}`);
+		}
+	}
 
     return (
         <S.Container>
             <S.ContainerCenter>
             <S.Title data-test="hashtag-title">#{hashtag}</S.Title>
-				<Timeline posts={posts} />
+            <InfiniteScroll
+					pageStart={0}
+					loadMore={loadMore}
+					hasMore={hasMoreItems}
+					loader={
+						<LoadingScroller key={0} />
+					}
+				>
+					<Timeline posts={posts} />
+				</InfiniteScroll>
 			</S.ContainerCenter>
 			<S.ContainerTrending>
 				<Trending />
